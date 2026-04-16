3ca0047eefc1e9fa33b1b25b5d8b45d4f7cc9fa8
MelCattell <33181468+MelCattell@users.noreply.github.com>
2026-04-16 11:20:31 +0100

    Initialize Terraform configuration for GCP

---
+++ main.tf
@@ -0,0 +1,18 @@
+variable "gcp_project" {}
+
+provider "google" {
+  project = var.gcp_project 
+  region  = "us-west1"
+}
+
+terraform {
+  backend "gcs" {
+    bucket  = "tf-remote-state-student_00_216119970206-22184-13669"
+    prefix  = "terraform/state/lab01" 
+  }
+}
+
+resource "google_compute_network" "example_vpc" {
+  name                    = "example-vpc"
+  auto_create_subnetworks = true 
+}
